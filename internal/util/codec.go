c73462f4df356fad3ebf662b3af6bde62f7bc39f
d-wojtasiewicz <dominikwojtasiewicz@gmail.com>
2024-10-09 19:40:34 +0100

    Add a decompress method

--- internal/util/codec.go
+++ internal/util/codec.go
@@ -3,6 +3,7 @@ package util
 import (
 	"bytes"
 	"compress/zlib"
+	"io"
 )
 
 func Compress(data []byte) ([]byte, error) {
@@ -16,5 +17,21 @@ func Compress(data []byte) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+	return buffer.Bytes(), nil
+}
+
+func Decompress(data []byte) ([]byte, error) {
+	reader, err := zlib.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	var buffer bytes.Buffer
+	_, err = io.Copy(&buffer, reader)
+	if err != nil {
+		return nil, err
+	}
+
 	return buffer.Bytes(), nil
 }
